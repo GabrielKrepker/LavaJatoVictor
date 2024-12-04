9e5c308d24d4b644f6843ee0df15e7dd15b02bc6
GabrielKrepker <135239611+GabrielKrepker@users.noreply.github.com>
2024-12-04 13:59:20 -0300

    Update urls.py

--- clientes/urls.py
+++ clientes/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('agendar/', views.agendar, name='agendar'),
     path('servicos/', views.servicos, name='servicos'),
+    path('login/', views.login_user, name='login'),
+    path('register/', views.register_user, name='register'),
+    path('agendar/', views.agendar, name='agendar'),
+    path('logout/', views.logout_user, name='logout_user')
 ]
