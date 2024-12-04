630378e3768cb462be176957a049f4ceb8174459
GabrielKrepker <135239611+GabrielKrepker@users.noreply.github.com>
2024-12-04 13:58:52 -0300

    Update admin.py

--- clientes/admin.py
+++ clientes/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
 from .models import Cliente, Carro, Funcionario, Agendamento
 
-admin.site.register(Cliente)
-admin.site.register(Carro)
-admin.site.register(Funcionario)
-admin.site.register(Agendamento)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'sobrenome', 'email', 'cpf')
+    search_fields = ('nome', 'sobrenome', 'email', 'cpf')
+
+class CarroAdmin(admin.ModelAdmin):
+    list_display = ('carro', 'placa', 'ano', 'cliente', 'lavagens', 'consertos')
+    search_fields = ('carro', 'placa')
+
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cargo', 'salario')
+    search_fields = ('nome', 'cargo')
+
+class AgendamentoAdmin(admin.ModelAdmin):
+    list_display = ('cliente', 'funcionario', 'data_hora', 'concluido')
+    search_fields = ('cliente__nome', 'funcionario__nome')
+
+admin.site.register(Cliente, ClienteAdmin)
+admin.site.register(Carro, CarroAdmin)
+admin.site.register(Funcionario, FuncionarioAdmin)
+admin.site.register(Agendamento, AgendamentoAdmin)
