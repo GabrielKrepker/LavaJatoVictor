6e1ce31d4b5020a09799e1b2e4fee51af5f78fa0
GabrielKrepker <135239611+GabrielKrepker@users.noreply.github.com>
2024-12-04 13:59:11 -0300

    Update models.py

--- clientes/models.py
+++ clientes/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Cliente(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='cliente')
     nome = models.CharField(max_length=50, null=False, blank=False)
     sobrenome = models.CharField(max_length=50, null=True, blank=True)
     email = models.EmailField(max_length=50, null=True, blank=True)
-    cpf = models.CharField(max_length=12, null=False, blank=False)  
+    cpf = models.CharField(max_length=12, null=False, blank=False, unique=True)
 
     def __str__(self) -> str:
         return self.nome
@@ -13,14 +15,14 @@ class Carro(models.Model):
     carro = models.CharField(max_length=50, null=False, blank=False)
     placa = models.CharField(max_length=9, null=False, blank=False)
     ano = models.IntegerField(null=True, blank=True)
-    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, null=False)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, null=False, default=1)
     lavagens = models.IntegerField(default=0)
     consertos = models.IntegerField(default=0)
     imagem = models.ImageField(upload_to='servicos/', null=True, blank=True)
 
     def __str__(self) -> str:
         return self.carro
-    
+
 class Funcionario(models.Model):
     nome = models.CharField(max_length=100, null=False, blank=False)
     cargo = models.CharField(max_length=100, null=False, blank=False)
@@ -28,7 +30,7 @@ class Funcionario(models.Model):
 
     def __str__(self):
         return self.nome
-    
+
 class Agendamento(models.Model):
     cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
     funcionario = models.ForeignKey(Funcionario, on_delete=models.CASCADE)
